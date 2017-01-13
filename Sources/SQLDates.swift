82705c8d3c076a2a5ffb80ba79c415404edf23e8
Ryan Collins <ryan@ryanmcollins.com>
2017-01-13 15:36:10 -0500

    Updated Specific SQL Date Types

--- Sources/SQLDates.swift
+++ Sources/SQLDates.swift
@@ -15,9 +15,23 @@ public func getSQLDate(_ date: Date) -> String {
     return formatter.string(from: date)
 }
 
-public func swiftDate(fromSQLstr str: String) -> Date? {
+public func getSQLDateTime(_ date: Date) -> String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
+    
+    return formatter.string(from: date)
+}
+
+public func getDate(fromSQLDate str: String) -> Date? {
     let formatter = DateFormatter()
     formatter.dateFormat = "yyyy-MM-dd"
+    
+    return formatter.date(from: str)
+}
+
+public func getDate(fromSQLDateTime str: String) -> Date? {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
     
     return formatter.date(from: str)
 }
